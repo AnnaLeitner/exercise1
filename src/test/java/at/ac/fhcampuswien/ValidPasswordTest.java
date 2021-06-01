0bd5292be7d0d18ec539ed0c97f92e3bbaf5b7f9
AnnaLeitner <71489265+AnnaLeitner@users.noreply.github.com>
2021-06-01 18:59:31 +0200

    Improvement and additional false tests

--- src/test/java/at/ac/fhcampuswien/ValidPasswordTest.java
+++ src/test/java/at/ac/fhcampuswien/ValidPasswordTest.java
@@ -19,60 +19,95 @@ public class ValidPasswordTest {
     }
 
     @Test
-    @DisplayName("Password length")
+    @DisplayName("Valid: Password length")
     public void validLengthTest(){
         ValidPassword pw = new ValidPassword();
-        assertTrue(pw.isValid("password"));
+        assertTrue(pw.isValid("password"), "Valid password");
     }
     @Test
-    @DisplayName("Too short")
+    @DisplayName("Invalid: Password length")
     public void tooShort(){
         ValidPassword pw = new ValidPassword();
-        assertFalse(pw.isValid("help"));
+        assertFalse(pw.isValid("short"),"Not a valid password");
     }
 
     @Test
-    @DisplayName("Check if string contains numbers and at least one Uppercase")
+    @DisplayName("Valid: Check if string contains numbers and at least one Uppercase")
     public void alphaNumberTest(){
         ValidPassword pw = new ValidPassword();
-        assertTrue(pw.isUpperLowerAlphaNumeric("Password123"));
+        assertTrue(pw.isUpperLowerAlphaNumeric("Password123"), "Valid password");
+    }
+    @Test
+    @DisplayName("Invalid: Check if string contains numbers and at least one Uppercase")
+    public void noAlphaNumberTest(){
+        ValidPassword pw = new ValidPassword();
+        assertFalse(pw.isUpperLowerAlphaNumeric("1111"), "Not a valid password");
     }
 
     @Test
-    @DisplayName("Special character check")
+    @DisplayName("Valid: Special character check")
     public void specialCharTest(){
         ValidPassword pw = new ValidPassword();
-        assertTrue(pw.specialChar("password()#$?!%/@"));
+        assertTrue(pw.specialChar("password()#$?!%/@"), "Valid password");
+    }
+    @Test
+    @DisplayName("Invalid: Special character check")
+    public void noSpecialCharTest(){
+        ValidPassword pw = new ValidPassword();
+        assertFalse(pw.specialChar(" {•◡•} "), "Not a valid password");
     }
 
     @Test
-    @DisplayName("Check Password")
+    @DisplayName("Valid: Check Password")
     public void passwordCheck(){
         ValidPassword pw = new ValidPassword();
-        assertTrue(pw.checkPassword("P4ssw0rd#"));
+        assertTrue(pw.checkPassword("P4ssw0rd#"), "Valid password");
+    }
+    @Test
+    @DisplayName("Invalid: Check Password")
+    public void falseCheckPassword(){
+        ValidPassword pw = new ValidPassword();
+        assertFalse(pw.checkPassword("Anna"), "Not a valid password");
     }
 
     //Erweiterung
     @Test
-    @DisplayName("Number occurs three times check")
+    @DisplayName("Valid: Number occurs three times check")
     public void occurThreeTimesTest(){
         ValidPassword pw = new ValidPassword();
-        assertTrue(pw.occurThree("password112211"));
+        assertTrue(pw.occurThree("password112211"), "Valid password");
     }
-
     @Test
-    @DisplayName("Ongoing Numbers check")
-    public void ongoingNumbersTest(){
+    @DisplayName("Invalid: Number occurs three times check")
+    public void occurMoreTimesTest(){
         ValidPassword pw = new ValidPassword();
-        assertTrue(pw.ongoingNumbers("p1s23"));
+        assertFalse(pw.occurThree("1111"), "Not a valid password");
     }
 
     @Test
-    @DisplayName("Check Password extension")
+    @DisplayName("Valid: Ongoing Numbers check")
+    public void twoOngoingNumbersTest(){
+        ValidPassword pw = new ValidPassword();
+        assertTrue(pw.ongoingNumbers("p1s23"), "Valid password");
+    }
+    @Test
+    @DisplayName("Invalid: Ongoing Numbers check")
+    public void moreOngoingNumbersTest(){
+        ValidPassword pw = new ValidPassword();
+        assertFalse(pw.ongoingNumbers("123"), "Not a valid password");
+    }
+
+    @Test
+    @DisplayName("Valid: Check Password extension")
     public void checkPassword2Test(){
         ValidPassword pw = new ValidPassword();
-        assertTrue(pw.checkPassword2("P4ssw0rd#12"));
+        assertTrue(pw.checkPassword2("P4ssw0rd#1112"), "Valid password");
     }
-
+    @Test
+    @DisplayName("Invalid: Check Password extension")
+    public void falseCheckPassword2Test(){
+        ValidPassword pw = new ValidPassword();
+        assertFalse(pw.checkPassword2("Anna123333"), "Not a valid password");
+    }
 
 }
